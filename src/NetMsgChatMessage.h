ee308520eeb3473aa12fe8a422501aac33861541
KSaunders98 <kirksaunders12@gmail.com>
2020-03-11 14:09:40 -0400

    Added networking

--- src/NetMsgChatMessage.h
+++ src/NetMsgChatMessage.h
@@ -2,26 +2,23 @@
 
 #include <string>
 
-#include "Mat4.h"
 #include "NetMsg.h"
-#include "Vector.h"
 
 #ifdef AFTR_CONFIG_USE_BOOST
 
 namespace Aftr {
 // message for updating other player's camera
-class NetMsgUpdateCamera : public NetMsg {
+class NetMsgChatMessage : public NetMsg {
 public:
-    NetMsgMacroDeclaration(NetMsgUpdateCamera);
+    NetMsgMacroDeclaration(NetMsgChatMessage);
 
-    NetMsgUpdateCamera();
+    NetMsgChatMessage();
     virtual bool toStream(NetMessengerStreamBuffer& os) const;
     virtual bool fromStream(NetMessengerStreamBuffer& is);
     virtual void onMessageArrived();
     virtual std::string toString() const;
 
-    Mat4 displayMatrix;
-    Vector position;
+    std::string msg;
 };
 }
 
